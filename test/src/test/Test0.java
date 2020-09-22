dc7870c5ca9b71be29ce0f22ea62ddc53cb85fb5
admin <admin@LAPTOP-8EDR72AT>
2020-09-22 16:37:54 +0800

    test

---
+++ test/src/test/Test0.java
@@ -0,0 +1,23 @@
+package test;
+
+public class Test0 {
+  int a = 0;	
+  public static void main(String[] args) {
+	
+	  System.out.println("test template");
+	  System.out.println("test template");
+//	  System.out.println();
+}
+  // TODO this method is not realized
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
